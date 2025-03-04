e9d10ac4c56a285d66f07182e606cdce49660c45
OscarGauss vaquita-fi <vaquita@blocky-labs.com>
2025-03-04 02:26:22 -0400

    fixing more page

--- src/vaquita-ui-submodule/components/pages/MorePage.tsx
+++ src/vaquita-ui-submodule/components/pages/MorePage.tsx
@@ -1,3 +1,12 @@
 export const MorePage = () => {
-  return <div>more</div>;
+  return (
+    <div>
+      <p className="text-lg">
+        Wanna learn more?
+      </p>
+      <p>
+        Checkout our social, or read our documentation to learn more about Vaquita
+      </p>
+    </div>
+  );
 };
